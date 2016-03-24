c4cce6181fa6732bf55a4213c9ab4d075f14badf
JackHillman <jack.hillman@me.com>
2016-03-24 22:24:46 +1030

    Init

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ Definition of urls for Amazsync.
 from datetime import datetime
 from django.conf.urls import patterns, url
 from app.forms import BootstrapAuthenticationForm
+from app import views
 
 # Uncomment the next lines to enable the admin:
 from django.conf.urls import include
@@ -13,9 +14,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'app.views.home', name='home'),
-    url(r'^contact$', 'app.views.contact', name='contact'),
-    url(r'^about', 'app.views.about', name='about'),
+    url(r'^$', views.home, name='home'),
+    url(r'^contact$', views.contact, name='contact'),
+    url(r'^about', views.about, name='about'),
+    url(r'^generator', views.gen, name='gen'),
+    url(r'^app/project/$', views.get_project_data, name="get_project_data"),
     url(r'^login/$',
         'django.contrib.auth.views.login',
         {
